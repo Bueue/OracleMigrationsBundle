3b84ffb4b666eedbc941e83c8848130fa00d68b9
Maurizio Moreo <moreoinformatica@gmail.com>
2017-09-06 22:14:53 +0200

    Method for materialized view name

--- MigrationsOraclePlatform.php
+++ MigrationsOraclePlatform.php
@@ -3,8 +3,32 @@
 namespace GOT\OracleMigrationsBundle;
 
 use Doctrine\DBAL\Platforms\OraclePlatform;
+use Doctrine\DBAL\Schema\Identifier;
 
 class MigrationsOraclePlatform extends OraclePlatform
 {
 
+    public function getMvName($prefix, $database)
+    {
+        if (!$database) {
+            throw new \InvalidArgumentException("Invalid database");
+        }
+
+        $database = $this->normalizeIdentifier($database);
+
+        $mvName = $prefix . '_' . $database->getName();
+
+        if (mb_strlen($mvName) > 30) {
+            throw new \LengthException("Materialized view name too long (\"$mvName\"), please choose shorter prefix");
+        }
+
+        return $mvName;
+    }
+
+    private function normalizeIdentifier($name)
+    {
+        $identifier = new Identifier($name);
+
+        return $identifier->isQuoted() ? $identifier : new Identifier(strtoupper($name));
+    }
 }
