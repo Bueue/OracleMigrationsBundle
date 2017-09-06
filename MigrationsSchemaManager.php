87442161f8162dff091f261f67acae916327919d
Maurizio Moreo <moreoinformatica@gmail.com>
2017-09-06 22:24:07 +0200

    Table details functions based on custom platform SQL

--- MigrationsSchemaManager.php
+++ MigrationsSchemaManager.php
@@ -10,4 +10,43 @@ class MigrationsSchemaManager extends OracleSchemaManager
     const MV_PREFIX_FKS  = 'MV_TAB_FKS';
     const MV_PREFIX_IDXS = 'MV_TAB_IDXS';
 
+    public function listTableColumns($table, $database = null)
+    {
+        if (!$database) {
+            $database = $this->_conn->getDatabase();
+        }
+
+        $mvTableColumnsName = $this->_platform->getMvName(self::MV_PREFIX_COLS, $database);
+
+        $sql = $this->_platform->getMvListTableColumnsSQL($mvTableColumnsName, $table);
+
+        $tableColumns = $this->_conn->fetchAll($sql);
+
+        return $this->_getPortableTableColumnList($table, $database, $tableColumns);
+    }
+
+    public function listTableForeignKeys($table, $database = null)
+    {
+        if (is_null($database)) {
+            $database = $this->_conn->getDatabase();
+        }
+
+        $mvTableForeignKeys = $this->_platform->getMvName(self::MV_PREFIX_FKS, $database);
+
+        $sql              = $this->_platform->getMvListTableForeignKeysSQL($mvTableForeignKeys, $table);
+        $tableForeignKeys = $this->_conn->fetchAll($sql);
+
+        return $this->_getPortableTableForeignKeysList($tableForeignKeys);
+    }
+
+    public function listTableIndexes($table)
+    {
+        $mvTableIndexes = $this->_platform->getMvName(self::MV_PREFIX_IDXS, $this->_conn->getDatabase());
+
+        $sql = $this->_platform->getMvListTableIndexesSQL($mvTableIndexes, $table);
+
+        $tableIndexes = $this->_conn->fetchAll($sql);
+
+        return $this->_getPortableTableIndexesList($tableIndexes, $table);
+    }
 }
